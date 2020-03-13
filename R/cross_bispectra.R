f3063e64269eb18fcd599c15a18e307a7bb2923d
Takeshi Abe <tabe@fixedpoint.jp>
2020-03-13 18:00:57 +0900

    Extend the domain of cross-bispectrum to the 1st and 4th quadrants

--- R/cross_bispectra.R
+++ R/cross_bispectra.R
@@ -15,12 +15,46 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-.generate_1st_region <- function(n) {
+## Return a data frame of frequency pairs in the 1st quadrant.
+## The unit of frequency x1 and x2 is cycles.
+##
+## @param n the number of samples
+.generate_1st_quadrant <- function(n) {
     stopifnot(length(n) == 1, n >= 1)
 
-    ymax <- function(x) ifelse(x <= n / 4, x, n / 2 - x)
-    xs <- 0:(n %/% 2)
-    do.call(rbind, Map(function(x, u) data.frame(x1 = x, x2 = 0:u), xs, ymax(xs)))
+    if (n < 4) {
+        data.frame(x1 = integer(), x2 = integer())
+    } else {
+        ymax <- function(x) n %/% 2 - x
+        xs <- seq_len(n %/% 2 - 1)
+        do.call(rbind, Map(function(x, u) data.frame(x1 = x, x2 = 1:u), xs, ymax(xs)))
+    }
+}
+
+## Return a data frame of frequency pairs in the 4th quadrant.
+## The x2 is positive, yet represents a negative frequency of the same absolute value.
+## Note that it excludes the row of x1 == x2, at which the third frequency is zero.
+##
+## @inheritParams .generate_1st_quadrant
+.generate_4th_quadrant <- function(n) {
+    stopifnot(length(n) == 1, n >= 1)
+
+    if (n < 4) {
+        data.frame(x1 = integer(), x2 = integer())
+    } else {
+        xs <- seq_len(n %/% 2 - 1)
+        do.call(rbind, Map(function(x, u) expand.grid(x1 = x, x2 = setdiff(xs, x)), xs))
+    }
+}
+
+## Return a data fram of frequency pairs in the 3rd region of Fig. 1 (a) in Lii and
+## Helland (1981). Like .generate_4th_quadrant(), the resulting x2 is positive, yet
+## represents a negative frequency of the same absolute value.
+##
+## @inheritParams .generate_1st_quadrant
+.generate_3rd_region <- function(n) {
+    data <- .generate_4th_quadrant(n)
+    data[data$x1 / 2 >= data$x2,]
 }
 
 #' Estimate cross-bispectrum from time series data.
@@ -30,9 +64,10 @@
 #' @param x Given 1st time series, as a data frame or matrix with which columns
 #' correspond to sampled stretches.
 #' @param y Given 2nd time series, with the same dimension as x.
-#' @param z Given 3rd time series, with the same dimension as x (and thus as y).
+#' @param z Optional 3rd time series, with the same dimension as x (and thus as y).
+#' If omitted, \code{y} is used instead.
 #' @param dft_given If TRUE, suppose that DFTs is given instead of time series
-#' data and skip the fast fourier transform.
+#' data and skip the fast fourier transform. Default: FALSE.
 #'
 #' @return A data frame including the following columns:
 #' \describe{
@@ -51,7 +86,7 @@
 #' K. S. Lii and K. N. Helland. 1981. Cross-Bispectrum Computation and Variance Estimation. ACM Trans. Math. Softw. 7, 3 (September 1981), 284–294. DOI:https://doi.org/10.1145/355958.35596
 #'
 #' @export
-cross_bispectrum <- function(x, y, z,
+cross_bispectrum <- function(x, y, z = y,
                              dft_given = FALSE) {
 
     ## Make data a matrix
@@ -77,16 +112,45 @@ cross_bispectrum <- function(x, y, z,
     dft_y <- if (dft_given) y else stats::mvfft(y)
     dft_z <- if (dft_given) z else stats::mvfft(z)
 
-    r1 <- .generate_1st_region(V)
+    q1 <- .generate_1st_quadrant(V)
 
-    value <- sapply(seq_len(nrow(r1)), function(i) {
-        f1 <- r1$x1[i] + 1
-        f2 <- r1$x2[i] + 1
-        f3 <- r1$x1[i] + r1$x2[i] + 1
+    v1 <- vapply(seq_len(nrow(q1)), function(i) {
+        f1 <- q1$x1[i] + 1
+        f2 <- q1$x2[i] + 1
+        f3 <- q1$x1[i] + q1$x2[i] + 1
         mean(dft_x[f1,] * dft_y[f2,] * Conj(dft_z[f3,])) / ((2 * pi)^2 * V)
-    })
+    }, complex(1))
 
-    data.frame(f1 = r1$x1 / V,
-               f2 = r1$x2 / V,
-               value = value)
+    if (identical(y, z)) {
+        r3 <- .generate_3rd_region(V)
+
+        v3 <- vapply(seq_len(nrow(r3)), function(i) {
+            f1 <- r3$x1[i] + 1
+            f2 <- r3$x2[i] + 1
+            f3 <- r3$x1[i] - r3$x2[i] + 1
+            mean(dft_x[f1,] * Conj(dft_y[f2,]) * Conj(dft_z[f3,])) / ((2 * pi)^2 * V)
+        }, complex(1))
+
+        data.frame(f1 = c(q1$x1, r3$x1) / V,
+                   f2 = c(q1$x2, -r3$x2) / V,
+                   value = c(v1, v3))
+    } else {
+        q4 <- .generate_4th_quadrant(V)
+
+        v4 <- vapply(seq_len(nrow(q4)), function(i) {
+            f1 <- q4$x1[i] + 1
+            f2 <- q4$x2[i] + 1
+            if (q4$x1[i] > q4$x2[i]) { # in the 3rd or 4th region
+                f3 <- q4$x1[i] - q4$x2[i] + 1
+                mean(dft_x[f1,] * Conj(dft_y[f2,]) * Conj(dft_z[f3,])) / ((2 * pi)^2 * V)
+            } else { # in the 5th or 6th region
+                f3 <- q4$x2[i] - q4$x1[i] + 1
+                mean(dft_x[f1,] * Conj(dft_y[f2,]) * dft_z[f3,]) / ((2 * pi)^2 * V)
+            }
+        }, complex(1))
+
+        data.frame(f1 = c(q1$x1, q4$x1) / V,
+                   f2 = c(q1$x2, -q4$x2) / V,
+                   value = c(v1, v4))
+    }
 }
